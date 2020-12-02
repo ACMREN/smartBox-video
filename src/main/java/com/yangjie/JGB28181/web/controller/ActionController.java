b966cb46c0b4741f400b2b7370108146eabad1b2
acmren <414443644@qq.com>
2020-12-02 16:12:26 +0800

    【新增】新增云台控制测试接口

--- src/main/java/com/yangjie/JGB28181/web/controller/ActionController.java
+++ src/main/java/com/yangjie/JGB28181/web/controller/ActionController.java
@@ -886,7 +886,7 @@ public class ActionController implements OnProcessListener {
 		System.out.println("lUserID,"+lUserID);
 
 		m_strClientInfo = new HCNetSDK.NET_DVR_CLIENTINFO();//预览参数 用户参数
-		m_strClientInfo.lChannel = new NativeLong(-1);
+		m_strClientInfo.lChannel = new NativeLong(1);
 
 		boolean result = hcNetSDK.NET_DVR_PTZControl_Other(lUserID, m_strClientInfo.lChannel, HCNetSDK.PAN_LEFT, 0);
 		System.out.println("result:" + result);
