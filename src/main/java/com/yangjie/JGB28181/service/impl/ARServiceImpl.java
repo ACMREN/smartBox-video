154748bd355677c27d46d3cf9f9d7af60dc6adbc
acmren <414443644@qq.com>
2021-02-01 15:12:16 +0800

    【修改】arServer修改为懒加载，加快启动速度

--- src/main/java/com/yangjie/JGB28181/service/impl/ARServiceImpl.java
+++ src/main/java/com/yangjie/JGB28181/service/impl/ARServiceImpl.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -33,7 +34,7 @@ import java.util.*;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.circle;
 
-
+@Lazy
 @Service("arService")
 public class ARServiceImpl implements IARService {
     Logger logger = LoggerFactory.getLogger(this.getClass());
